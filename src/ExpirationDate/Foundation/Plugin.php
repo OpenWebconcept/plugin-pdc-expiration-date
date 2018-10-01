abb7feaa9e193332651ef429a9bde3347a968dd6
Edwin Siebel <edwin@yard.nl>
2018-10-01 22:16:33 +0200

    Update: languages [skip ci]

--- src/ExpirationDate/Foundation/Plugin.php
+++ src/ExpirationDate/Foundation/Plugin.php
@@ -1,16 +1,22 @@
 <?php
+/**
+ * The base of the plugin.
+ */
 
 namespace OWC\PDC\ExpirationDate\Foundation;
 
 use OWC\PDC\Base\Foundation\Plugin as BasePlugin;
 
+/**
+ * Sets the name and version of the plugin.
+ */
 class Plugin extends BasePlugin
 {
 
     /**
      * Name of the plugin.
      *
-     * @var string
+     * @const string NAME
      */
     const NAME = 'pdc-expiration-date';
 
@@ -18,7 +24,7 @@ class Plugin extends BasePlugin
      * Version of the plugin.
      * Used for setting versions of enqueue scripts and styles.
      *
-     * @var string
+     * @const string VERSION
      */
     const VERSION = '1.0.0';
 
